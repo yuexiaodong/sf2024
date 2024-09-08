d676cfdda1d113bbbffe39eee1657417505861c1
yuexiaodong <yuexiaodong2011@163.com>
2024-09-08 23:03:04 +0800

    array

--- src/main/java/com/sf/ArraySfTest.java
+++ src/main/java/com/sf/ArraySfTest.java
@@ -9,6 +9,62 @@ public class ArraySfTest {
 
     }
 
+    // lc11:盛最多水的容器
+    // 1：暴力法  2：双指针法
+    public int maxArea(int[] heights) {
+        int left = 0;
+        int right = heights.length - 1;
+        int max = 0;
+        while (left < right) {
+            int area =  (right - left) * Math.min(heights[left], heights[right]);
+            if (area > max) {
+                max = area;
+            }
+            if (heights[left]  < heights[right]) {
+                left ++;
+            } else {
+                right --;
+            }
+        }
+        return max;
+    }
+
+
+    // lc :1  hash表
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(target - nums[i])) {
+                map.put(nums[i], i);
+            } else {
+                ans[0] = i;
+                ans[1] = map.get(target - nums[i]);
+                return ans;
+            }
+        }
+        return new int[0];
+    }
+
+    //  lc167： 非递减数组， 有序数组可以考虑二分查找; 也可以使用双指针逼近
+    public int[] twoSumV2(int[] nums, int target) {
+        for (int  i = 0;  i < nums.length;  i++) {
+            int low  = i + 1;
+            int high = nums.length - 1;
+            while (low <= high) {
+                int mid = (high - low ) / 2 + low;
+                if (nums[mid] + nums[i] == target) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (nums[mid] + nums[i] > target) {
+                    high = mid - 1;
+                } else {
+                    low  = mid + 1;
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
     // lc189:轮转数组  暴力解法
     public void rotate(int[] nums, int k) {
         int len = nums.length;
--- src/main/java/com/sf/LinkedListTest.java
+++ src/main/java/com/sf/LinkedListTest.java
@@ -2,7 +2,6 @@ package com.sf;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class LinkedListTest {
